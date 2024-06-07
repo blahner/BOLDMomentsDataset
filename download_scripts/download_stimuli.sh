de83139d5f94c31194276787278975a8ae6f0def
Benjamin M Lahner <blahner@visiongpu51.csail.mit.edu>
2024-06-07 09:29:01 -0400

    edit stimuli download script

--- download_scripts/download_stimuli.sh
+++ download_scripts/download_stimuli.sh
@@ -1,16 +1,37 @@
+#!/bin/bash
 set -e
 #local absolute path to where you want to download the dataset
-LOCAL_DIR=/data/vision/oliva/scratch/blahner/BMD_tmp
-mkdir ${LOCAL_DIR}
-mkdir ${LOCAL_DIR}/derivatives
-mkdir ${LOCAL_DIR}/derivatives/stimuli_metadata
-mkdir ${LOCAL_DIR}/derivatives/stimuli_metadata/mp4_h264
+LOCAL_DIR="/your/path/to/BOLDMomentsDataset"
 
-aws s3 sync --no-sign-request s3://openneuro.org/ds005032/derivatives/stimuli_metadata/mp4_h264 \
-    ${LOCAL_DIR}/derivatives/stimuli_metadata/mp4_h264/
-dataset_files=("annotations.json")
+#create directory paths that mimic the openneuro dataset structure
+mkdir -p "${LOCAL_DIR}/derivatives/stimuli_metadata"
 
-for f in "${dataset_files[@]}"; do
-    aws s3 cp --no-sign-request s3://openneuro.org/ds005032/derivatives/stimuli_metadata/${f} \
-    ${LOCAL_DIR}/stimuli_metadata/
+#download the folders
+dataset_folders=(
+    "mp4_h264"
+    "stimuli"
+    "frames_middle"
+    "ME_feats_matlab"
+)
+for folder in "${dataset_folders[@]}"; do
+    mkdir -p "${LOCAL_DIR}/derivatives/stimuli_metadata/${folder}"
+
+    aws s3 cp --no-sign-request --recursive \
+    "s3://openneuro.org/ds005165/derivatives/stimuli_metadata/${folder}/" \
+    "${LOCAL_DIR}/derivatives/stimuli_metadata/${folder}/"
+done
+
+#download the files
+dataset_files=(
+    "README.txt"
+    "annotations.json"
+    "llm_frame_annotations.json"
+    "annotations_fieldnames.json"
+    "frames.zip"
+    )
+
+for file in "${dataset_files[@]}"; do
+    aws s3 cp --no-sign-request \
+    "s3://openneuro.org/ds005165/derivatives/stimuli_metadata/${file}" \
+    ${LOCAL_DIR}/derivatives/stimuli_metadata/
 done
